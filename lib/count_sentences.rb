ae94db24c3706037668628f2293d51b8a67a2cc9
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-07 22:16:03 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -30,7 +30,8 @@ class String
   end
 
   def count_sentences
-    new_array = self.split(/[.?!]/).reject {|string|string.empty}
+    new_array = self.split(/[.?!]/).reject {|string|string.empty?}
+    return new_array.length
 
   end
 end
